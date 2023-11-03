eb0f9e4e7e6098f90a5b6e3653108181233ab989
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-11-02 21:29:34 -0300

    Project created

---
+++ src/main/java/tech/vicko/VickoApplication.java
@@ -0,0 +1,13 @@
+package tech.vicko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VickoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VickoApplication.class, args);
+	}
+
+}
